a8ac71125af089e0f2b2e7cffeb9920b6ab59847
Michael Mena <michael.mena2408@gmail.com>
2023-06-02 14:21:39 -0300

    u

--- src/main/java/com/example/crud_transacciones/modelo/Conexion.java
+++ src/main/java/com/example/crud_transacciones/modelo/Conexion.java
@@ -19,32 +19,42 @@ public class Conexion {
         if (dataSource == null) {
             try {
                 Properties props = new Properties();
-//                InputStream is;
 
-//                String environment = System.getProperty("/etc/secrets/production.env");
-                InputStream secrets = Thread.currentThread().getContextClassLoader().getResourceAsStream("/etc/secrets/production.env");
-/*                if(environment != null) {
-                    props.load(new FileInputStream(environment));
-                } else {
-                    props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("secret.properties"));
-                }*/
-                if(secrets == null) {
+/*                String productionEnvPath = "/etc/secrets/production.env";
+                InputStream secrets = Thread.currentThread().getContextClassLoader().getResourceAsStream(productionEnvPath);*/
+
+/*                if (secrets == null) {
                     props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("secret.properties"));
                 } else {
                     props.load(secrets);
-                }
-                props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+                }*/
+
+/*                props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
                 String URL = props.getProperty("db.url");
                 String username = props.getProperty("db.username");
-                String password = props.getProperty("db.password");
+                String password = props.getProperty("db.password");*/
+
+                props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+
+                // Obtener las variables de entorno de RENDER
+                String url = System.getenv("db.url");
+                String username = System.getenv("db.username");
+                String password = System.getenv("db.password");
+
+                // Si las variables de entorno de RENDER están presentes, usarlas en lugar de las del archivo de propiedade
+                if( url == null || username == null || password == null ) {
+                    props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("secret.properties"));
+                    url = props.getProperty("db.url");
+                    username = props.getProperty("db.username");
+                    password = props.getProperty("db.password");
+                }
 
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 dataSource = new BasicDataSource();
-                dataSource.setUrl(URL);
+                dataSource.setUrl(url);
                 dataSource.setUsername(username);
                 dataSource.setPassword(password);
                 dataSource.setInitialSize(5);
-
             } catch (IOException ex) {
                 throw new RuntimeException("Error al cargar el archivo de propiedades", ex);
             } catch (ClassNotFoundException ex) {
